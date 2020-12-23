466a54a17a37a1c286a1e4c6501cbf19771ea974
redwan zia <redwan.zia@gmail.com>
2020-12-23 11:38:50 -0500

    finished tour project

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project';
+function App() {
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ tours, setTours ] = useState([]);
+
+	const removeTour = (id) => {
+		const newTours = tours.filter((tour) => tour.id !== id);
+		setTours(newTours);
+	};
+
+	const fetchTours = async () => {
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setIsLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className='title'>
+					<h2>no tour left</h2>
+					<button onClick={() => fetchTours()} className='btn'>
+						refresh
+					</button>
+				</div>
+			</main>
+		);
+	}
+
+	return (
+		<main>
+			<Tours tours={tours} removeTour={removeTour} />
+		</main>
+	);
+}
+
+export default App;
